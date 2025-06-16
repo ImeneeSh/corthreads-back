b6a4e796bf7bee6d8e9409b642e0c8c139941f4d
ImeneeSh <nourelimene.sahi@se.univ-bejaia.dz>
2025-06-16 19:46:42 +0100

    first commit

---
+++ src/main/java/org/example/Connexion.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+class Connexion {
+    public final static String URL="jdbc:mysql://localhost:3306/corthreads";
+    public final static String USER="root";
+    public final static String PASSWORD="";
+
+    static{
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnexion() throws SQLException {
+        return DriverManager.getConnection(URL,USER,PASSWORD);
+    }
+}
